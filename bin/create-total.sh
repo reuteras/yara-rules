3c9601d07784c31c72ed04445d3ec1376afe7b39
Peter Reuterås <peter@reuteras.com>
2020-05-06 21:00:00 +0200

    Add new submodule and fix rules for it.

--- bin/create-total.sh
+++ bin/create-total.sh
@@ -8,4 +8,10 @@ find -E misc sub -regex ".*\.yara?" | \
     # Remove duplicte rules
     grep -vE "malware-ioc/turla/(carbon|gazer)\.yar" | \
     grep -vE "malware-ioc/groundbait/prikormka.yar" | \
+    grep -vE "Malware-Misc-RE/2020-04-18-maze-ransomware-unpacked-payload.vk.yar" | \
+    # Remove file with <fs>...</fs> tag
+    grep -vE "Malware-Misc-RE/2020-03-27-dridex-worker-config-software-banking-yara.vk.yar" | \
+    # Remove files with errors
+    grep -vE "Malware-Misc-RE/2020-04-07-qbot-qsort-miniupnp-vk.yar" | \
+    grep -vE "Malware-Misc-RE/2020-03-19-netwalker-yara-config-yar-vk.yar" | \
     xargs cat > total/total.yara
