795b1e18ff71e0858a1df967be05ccfe3ba066ed
Peter Reuterås <peter@reuteras.com>
2020-05-17 17:41:21 +0200

    Update create file

--- bin/create-total.sh
+++ bin/create-total.sh
@@ -4,14 +4,19 @@
 
 find -E misc sub -regex ".*\.yara?" -print0 | \
     # Remove rules specific for LOKI and SPARK, https://github.com/Neo23x0/signature-base#external-variables-in-yara-rules
-    grep -Ev "yara/(generic_anomalies|general_cloaking|thor_inverse_matches|yara_mixed_ext_vars)\.yar" | \
+    sed -E "s#sub/signature-base/yara/(generic_anomalies|general_cloaking|thor_inverse_matches|yara_mixed_ext_vars)\.yar##g" | \
+    sed -E "s#sub/signature-base/yara/apt_turla_penquin.yar##" | \
     # Remove duplicte rules
-    grep -vE "malware-ioc/turla/(carbon|gazer)\.yar" | \
-    grep -vE "malware-ioc/groundbait/prikormka.yar" | \
-    grep -vE "Malware-Misc-RE/2020-04-18-maze-ransomware-unpacked-payload.vk.yar" | \
+    sed -E "s#sub/malware-ioc/turla/(carbon|gazer)\.yar##g" | \
+    sed -E "s#sub/malware-ioc/groundbait/prikormka.yar##" | \
+    sed -E "s#sub/Malware-Misc-RE/2020-04-18-maze-ransomware-unpacked-payload.vk.yar##" | \
     # Remove file with <fs>...</fs> tag
-    grep -vE "Malware-Misc-RE/2020-03-27-dridex-worker-config-software-banking-yara.vk.yar" | \
+    sed -E "s#sub/Malware-Misc-RE/2020-03-27-dridex-worker-config-software-banking-yara.vk.yar##" | \
     # Remove files with errors
-    grep -vE "Malware-Misc-RE/2020-04-07-qbot-qsort-miniupnp-vk.yar" | \
-    grep -vE "Malware-Misc-RE/2020-03-19-netwalker-yara-config-yar-vk.yar" | \
+    sed -E "s#sub/Malware-Misc-RE/2020-04-07-qbot-qsort-miniupnp-vk.yar##" | \
+    sed -E "s#sub/Malware-Misc-RE/2020-03-19-netwalker-yara-config-yar-vk.yar##" | \
+    sed -E "s#sub/APT_REPORT/Turla/2017/PENQUIN_MOONLIT_MAZE.yara##" | \
+    sed -E "s#sub/APT_REPORT/APT28/yara/APT28.yar##" | \
+    sed -E "s#sub/APT_REPORT/Oceanlotus/oceanlotus_png_loader.yar##" | \
+    tr -d '\n' | \
     xargs -0 cat > total/total.yara
